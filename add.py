82654e895953a1a9e82c4753d1f6ef61b26d2e21
Priti Maurya <pritimauryaa21@gmail.com>
2026-06-29 18:18:18 +0530

    Fix input prompt formatting in add.py

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
-a = int(input("Enter 1st number": ))
-b = int(input("Enter 2nd number": ))
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
 
 print("Addition of ", a, " and ", b, " is ", a+b)
 
